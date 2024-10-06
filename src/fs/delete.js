73b85c5530bcc7997dfe784a02e05f3f601b4ad7
Podexus23 <santon23@gmail.com>
2024-10-06 21:23:07 +0300

    refactor: simplify delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -7,15 +7,16 @@ const __dirname = dirname(__filename);
 
 const remove = async () => {
   const fileSrc = path.join(__dirname, "files", "fileToRemove.txt");
+
   try {
-    await fs.access(fileSrc);
     await fs.unlink(fileSrc);
+    console.log("File deleted")
   } catch (error) {
-    if (error.code == "ENOENT")
-      throw new Error(`FS operation failed: src file don't exists`);
-    else console.log(error);
-    return 1;
+    console.log(error);
+    throw new Error(`FS operation failed: ${error.message}`);
   }
+
+
 };
 
 await remove();
